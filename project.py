30cb5503bbbadd7e87e469d9113dc8b1a459ea5f
TanishqRana <52535733+TanishqRana@users.noreply.github.com>
2021-03-18 20:30:57 +0530

    initial commit

---
+++ project.py
@@ -0,0 +1,155 @@
+import requests
+import json
+import pyttsx3
+import speech_recognition as sr
+import re
+import threading
+import time
+
+API_KEY = "t7KB0QTKhDyW"
+PROJECT_TOKEN = "tdxRRLeLaVCs"
+RUN_TOKEN = "tSaYTcvBDRjm"
+
+
+
+
+class Data:
+    def __init__(self, api_key, project_token):
+        self.api_key = api_key
+        self.project_token=project_token
+        self.params={
+            "api_key":self.api_key
+        }
+        self.data =self.get_data()
+        
+    def get_data(self):
+        response = requests.get("https://www.parsehub.com/api/v2/projects/"+self.project_token+"/last_ready_run/data"
+        , params=self.params)
+        data = json.loads(response.text)
+        return data
+		
+    def get_total_cases(self):
+        data=self.data["total"]
+        for content in data:
+            if content["name"]== "Coronavirus Cases:":
+                return content["value"]
+
+    def get_total_deaths(self):
+        data=self.data["total"]
+        for content in data:   
+            if content["name"]== "Deaths:":
+                return content["value"]
+
+    def get_country_data(self, country):
+        data = self.data["country"]
+        for content in data:
+            if content["name"].lower()== country.lower():
+                return content
+        return "0"
+    
+    def get_list_of_countries(self):
+        countries=[]
+        for country in self.data["country"]:
+            countries.append(country["name"].lower())
+        return countries
+
+    def update_data(self):
+        response = requests.post("https://www.parsehub.com/api/v2/projects/"+self.project_token+"/run", params=self.params)
+		
+
+        def poll():
+            time.sleep(0.1)
+            old_data = self.data
+            while True:
+                new_data = self.get_data()
+                if new_data != old_data:
+                    self.data= new_data
+                    print("Data Updated")
+                    break
+                time.sleep(5)
+
+        t = threading.Thread(target=poll)
+        t.start()
+
+
+
+
+def speak(text):
+    engine = pyttsx3.init()
+    engine.say(text)
+    engine.runAndWait()
+
+def get_audio():
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        audio = r.listen(source)
+        said=""
+        try:
+            said = r.recognize_google(audio)
+        except Exception as e:
+            print("Exception:" , str(e))
+    return said.lower()
+
+
+
+def main():
+    print("Started program")
+    data = Data(API_KEY,PROJECT_TOKEN)
+    END_PHRASE = "stop"
+    country_list = data.get_list_of_countries()
+
+    TOTAL_PATTERNS = {
+                        re.compile("[\w\s]+ total [\w\s]+ cases"):data.get_total_cases,
+                        re.compile("total [\w\s]+ cases"):data.get_total_cases,
+					    re.compile("[\w\s]+ total cases"): data.get_total_cases,
+                         re.compile("total cases +[\w\s] "): data.get_total_cases,
+                        re.compile("total cases"): data.get_total_cases,
+                        re.compile("[\w\s]+ total [\w\s]+ deaths"): data.get_total_deaths,
+                        re.compile("total [\w\s]+ deaths"): data.get_total_deaths,
+                        re.compile("[\w\s]+ total deaths"): data.get_total_deaths,
+                        re.compile("total deaths+[\w\s] "): data.get_total_deaths,
+                        re.compile("total deaths"): data.get_total_deaths,
+                        re.compile("[\w\s]+ total [\w\s]+ death"): data.get_total_deaths,
+                        re.compile("total [\w\s]+ death"): data.get_total_deaths,
+                        re.compile("[\w\s]+ total death"): data.get_total_deaths,
+                        re.compile("total death+[\w\s] "): data.get_total_deaths,
+                        re.compile("total death"): data.get_total_deaths
+                    } 
+    COUNTRY_PATTERNS={
+                        re.compile("[\w\s]+ cases [\w\s]+"): lambda country: data.get_country_data(country)['total_cases'],
+                        re.compile("[\w\s]+ deaths [\w\s]+"): lambda country: data.get_country_data(country)['total_deaths'],
+                        re.compile("[\w\s]+ death [\w\s]+"): lambda country: data.get_country_data(country)['total_deaths']
+                    }
+
+    UPDATE_COMMAND = "update"
+    while True:
+        print("Listening...")
+        text = get_audio()
+        print(text)
+        result = None
+
+        for pattern , func in COUNTRY_PATTERNS.items():
+            if pattern.match(text):
+                for country in country_list:
+                    if re.search(country,text):
+                        result=func(country)
+                        break
+                    
+        if result==None:                
+            for pattern , func in TOTAL_PATTERNS.items():
+                if pattern.match(text):
+                    result=func()
+
+        if text == UPDATE_COMMAND:
+            result = "Data is being updated. This may take a moment!"
+            data.update_data()
+
+        if result:
+            print(result)
+            speak(result)
+
+        if text.find(END_PHRASE)!=-1:
+            print("Exit")
+            break
+
+main()
